7157c83525b4b33ae55d9d0962020d42111673b3
Michael Pelz-Sherman <mpelzsherman@primedia.com>
2015-07-25 23:30:26 -0400

    Extend calculator to support arbitrary operand limits, defaulting to 2.

--- lib/tioneb.rb
+++ lib/tioneb.rb
@@ -2,16 +2,18 @@ require_relative "input_validator"
 
 class Tioneb
 
-  def initialize
+  def initialize(operand_limit=2)
     @stack = []
-    @iv = InputValidator.new
+    @input_validator = InputValidator.new
+    @operand_limit = operand_limit
   end
 
   def process_input(input)
-    if @iv.is_float?(input)
+    if @input_validator.is_float?(input)
       @stack.push(Float(input))
-    elsif @iv.is_operator?(input)
-      @stack = Array(@stack.reduce(input.to_sym))
+    elsif @input_validator.is_operator?(input)
+      operands = @stack.pop(@operand_limit)
+      @stack.push(operands.reduce(input.to_sym))
     else
       raise ArgumentError, "Sorry, I don't know how to process #{input}."
     end
--- test/tioneb_test.rb
+++ test/tioneb_test.rb
@@ -2,7 +2,7 @@ require 'tioneb'
 
 RSpec.describe Tioneb, "#process_input" do
 
-  subject { Tioneb.new }
+  subject { Tioneb.new(2) }
 
   it "should raise an exception given invalid input" do
     expect { subject.process_input('a') }.to raise_error(ArgumentError)
@@ -12,13 +12,6 @@ RSpec.describe Tioneb, "#process_input" do
     expect(subject.process_input('5')).to eq 5.0
     expect(subject.process_input('8')).to eq 8.0
     expect(subject.process_input('+')).to eq 13.0
-  end
-
-  it "should add 3 numbers" do
-    expect(subject.process_input('5')).to eq 5.0
-    expect(subject.process_input('8')).to eq 8.0
-    expect(subject.process_input('13')).to eq 13.0
-    expect(subject.process_input('+')).to eq 26.0
   end
 
   it "should multiply numbers" do
@@ -36,16 +29,15 @@ RSpec.describe Tioneb, "#process_input" do
   it "should subtract numbers" do
     expect(subject.process_input('24')).to eq 24.0
     expect(subject.process_input('4')).to eq 4.0
-    expect(subject.process_input('1')).to eq 1.0
-    expect(subject.process_input('-')).to eq 19.0
+    expect(subject.process_input('-')).to eq 20.0
   end
 
   it "should replace the operands with the operation result" do
-    expect(subject.process_input('-3')).to eq -3.0
-    expect(subject.process_input('-2')).to eq -2.0
-    expect(subject.process_input('*')).to eq 6.0
-    expect(subject.process_input('5')).to eq 5.0
-    expect(subject.process_input('+')).to eq 11.0
+    expect(subject.process_input('2')).to eq 2.0
+    expect(subject.process_input('3')).to eq 3.0
+    expect(subject.process_input('9')).to eq 9.0
+    expect(subject.process_input('+')).to eq 12.0
+    expect(subject.process_input('*')).to eq 24.0
   end
 
 end
